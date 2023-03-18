831baa433d6ed65de60e61adb13c006b897a912c
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-03-18 14:46:24 +0700

    Update muon.sh

--- muon/muon.sh
+++ muon/muon.sh
@@ -1,3 +1,46 @@
+PS3="Choose option and press Enter"
+options=("Install Muon", "Backup")
+select opt in "${options[@]}"
+do
+    case $opt in
 
-sudo apt update && sudo apt upgrade -y
+        ############ Installing Muon ############
+        "Install Muon")
+            echo -e '\n\e[42mInstalling Muon...\e[0m\n' && sleep 1
+
+            # update && upgrade system
+            sudo apt update && sudo apt upgrade -y
+
+            # install dependencies
+            sudo apt install ca-certificates curl gnupg lsb-release -y
+
+            # install Docker
+            sudo apt update
+            sudo mkdir -m 0755 -p /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt update
+            sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+            # download Muon docker-compose.yml
+            curl -o docker-compose.yml https://raw.githubusercontent.com/muon-protocol/muon-node-js/testnet/docker-compose-pull.yml
+
+            # run container
+            docker compose up -d
+
+        ;;
+
+        ############ Creating A Backup ############
+        "Backup")
+            echo -e '\n\e[42mCreating a backup...\e[0m\n' && sleep 1
+
+            docker exec -it muon-node ./node_modules/.bin/ts-node ./src/cmd keys backup > backup.json
+
+
+      esac  
+
+done
+
 

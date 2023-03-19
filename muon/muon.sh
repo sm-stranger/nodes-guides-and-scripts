d797f472b57ac425ad2a63834c5cf3d89b59ad45
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-03-20 00:27:49 +0700

    Update muon.sh

--- muon/muon.sh
+++ muon/muon.sh
@@ -1,7 +1,3 @@
-muon="./muon.sh"
-#echo 'export muon='$muon >> $HOME/.profile
-#source $HOME/.profile
-
 PS3="Choose option and press Enter: "
 options=("Install Muon" "Create Backup" "Check Node Status" "Show Logs")
 select opt in "${options[@]}"
@@ -79,9 +75,4 @@ do
 
 done
 
-# logs
 # docker logs -f --tail 50 muon-node
-
-
-
-curl -s https://raw.githubusercontent.com/nodersteam/cosmos

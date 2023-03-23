f1fd760e66845dded1c387676aa93fb974f75730
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-03-24 01:58:46 +0700

    Update starknet.sh

--- starknet/starknet.sh
+++ starknet/starknet.sh
@@ -4,13 +4,14 @@ while true
 do
 
 PS3="Choose option and press Enter: "
-options=("Install Starknet" "Create Backup" "Check Node Status" "Show Logs" "Exit")
+options=("Install" "Delete" "Update" "Check Logs")
 select opt in "${options[@]}"
 do
     case $opt in
     
-        ######################################## Install Starknet ########################################
-        "Install Starknet")
+        ######################################## Install ########################################
+        
+        "Install")
             
             # update && upgrade
             sudo apt update && sudo apt upgrade -y
@@ -41,32 +42,79 @@ do
             source $HOME/.bash_profile
             mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 
-            echo "
-                [Unit]
-                    Description=StarkNet
-                    After=network.target
-                [Service]
-                    User=$USER
-                    Type=simple
-                    WorkingDirectory=$HOME/pathfinder/py
-                    ExecStart=/bin/bash -c \"source $HOME/pathfinder/py/.venv/bin/activate && /usr/local/bin/pathfinder --http-rpc=\"0.0.0.0:9545\" --ethereum.url $ALCHEMY\"
-                    Restart=on-failure
-                    LimitNOFILE=65535
-                [Install]
-                    WantedBy=multi-user.target" > $HOME/starknetd.service
-                    mv $HOME/starknetd.service /etc/systemd/system/
-
+            echo "[Unit]
+            Description=StarkNet
+            After=network.target
+            [Service]
+            User=$USER
+            Type=simple
+            WorkingDirectory=$HOME/pathfinder/py
+            ExecStart=/bin/bash -c \"source $HOME/pathfinder/py/.venv/bin/activate && /usr/local/bin/pathfinder --http-rpc=\"0.0.0.0:9545\" --ethereum.url $ALCHEMY\"
+            Restart=on-failure
+            LimitNOFILE=65535
+            [Install]
+            WantedBy=multi-user.target" > $HOME/starknetd.service
+            mv $HOME/starknetd.service /etc/systemd/system/
 
             sudo systemctl restart systemd-journald
             sudo systemctl daemon-reload
             sudo systemctl enable starknetd
             sudo systemctl restart starknetd
+                  
+        break
+        ;;
 
 
-            
+        ######################################## Delete ########################################
+
+        "Delete")
+        
+        systemctl stop starknetd
+        systemctl disable starknetd
+        rm -rf ~/pathfinder/
+        rm -rf /etc/systemd/system/starknetd.service
+        rm -rf /usr/local/bin/pathfinder
         
         break
         ;;
+
+
+        ######################################## Update ########################################
+        
+        "Update")
+
+            cd ~/pathfinder
+            rustup update
+            git fetch
+            git checkout v0.5.1
+            cargo build --release --bin pathfinder
+            mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+            cd py
+            source .venv/bin/activate
+            PIP_REQUIRE_VIRTUALENV=true pip install -e .[dev]
+            pip install --upgrade pip
+            systemctl restart starknetd
+
+        break
+        ;;
+
+
+        ######################################## Check Logs ########################################
+
+        "Check Logs")
+            journalctl -u starknetd -f
+        break
+        ;;
+
+
+        ######################################## Restart ########################################
+
+        "Restart")
+            systemctl restart starknetd
+        break
+        ;;
+
+
     
     esac
 

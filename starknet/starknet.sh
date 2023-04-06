3fc60cf5284d84590f2adeecafd15cdff4176ca8
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-04-06 16:08:53 +0700

    Update starknet.sh

--- starknet/starknet.sh
+++ starknet/starknet.sh
@@ -53,17 +53,17 @@ do
             source $HOME/.bash_profile
 
             echo "[Unit]
-Description=StarkNet
-After=network.target
-[Service]
-User=$USER
-Type=simple
-WorkingDirectory=$HOME/pathfinder/py
-ExecStart=/bin/bash -c \"source $HOME/pathfinder/py/.venv/bin/activate && /usr/local/bin/pathfinder --http-rpc=\"0.0.0.0:9545\" --ethereum.url $ALCHEMY\"
-Restart=on-failure
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target" > $HOME/starknetd.service
+            Description=StarkNet
+            After=network.target
+            [Service]
+            User=$USER
+            Type=simple
+            WorkingDirectory=$HOME/pathfinder/py
+            ExecStart=/bin/bash -c \"source $HOME/pathfinder/py/.venv/bin/activate && /usr/local/bin/pathfinder --http-rpc=\"0.0.0.0:9545\" --ethereum.url $ALCHEMY\"
+            Restart=on-failure
+            LimitNOFILE=65535
+            [Install]
+            WantedBy=multi-user.target" > $HOME/starknetd.service
             mv $HOME/starknetd.service /etc/systemd/system/
 
             sudo systemctl restart systemd-journald
@@ -96,7 +96,7 @@ WantedBy=multi-user.target" > $HOME/starknetd.service
             cd ~/pathfinder
             rustup update
             git fetch
-            git checkout v0.5.1
+            git checkout v0.5.2
             source $HOME/.cargo/env
             cargo build --release --bin pathfinder
             mv ~/pathfinder/target/release/pathfinder /usr/local/bin/

1b486e756ee1eacff2eb58050ff6fba788f442ce
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-05-20 16:31:15 +0700

    Update aleo_dsc.sh

--- aleo/aleo_dsc.sh
+++ aleo/aleo_dsc.sh
@@ -8,8 +8,8 @@ fi
 sudo apt update && sudo apt upgrade -y
 sudo apt install make clang pkg-config libssl-dev build-essential gcc xz-utils git curl vim tmux ntp jq llvm ufw -y
 
-curl -s https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/functions.sh
-
+wget -O functions.sh https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/functions.sh
+source functions.sh
 
 enter_val "Private Key" PK
 enter_val "View Key" VK

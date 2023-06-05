f3e9361f9f4d427cf822a251abebe8394202daca
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-06-05 14:30:31 +0700

    Update aleo_sc_test.sh

--- aleo/aleo_sc_test.sh
+++ aleo/aleo_sc_test.sh
@@ -1,49 +1,50 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
+sudo curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf --nightly | sh
+
 sudo apt install make clang pkg-config libssl-dev build-essential gcc xz-utils git curl vim tmux ntp jq llvm ufw mc -y
 
-#if ! [ -f /root/fn.sh ]; then
-#    wget -O fn.sh https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh
-#fi
-#source fn.sh
-
 #curl -s https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh
-
-
-if [ -z "$PK" ]; then 
-    read -p "Private Key: " PK
-    echo 'export PK='${PK} >> $HOME/.bash_profile
+if ! [ -f /root/fn.sh ]; then
+    wget -O fn.sh https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh
 fi
-if [ -z "$VK" ]; then 
-    read -p "View Key: " VK
-    echo 'export VK='${VK} >> $HOME/.bash_profile
-fi
-if [ -z "$ADDRESS" ]; then 
-    read -p "Address: " ADDRESS
-    echo 'export ADDRESS='${ADDRESS} >> $HOME/.bash_profile
-fi
+source fn.sh
 
 
+# install snarkOS
 if ! [ -d /root/snarkOS ]; then
-    cd && git clone https://github.com/AleoHQ/snarkOS.git --depth 1
-    cd $HOME/snarkOS
-    bash ./build_ubuntu.sh
+    cd && git clone https://github.com/AleoHQ/snarkOS.git && cd snarkOS
+    git pull
+    cargo build
 fi
-source $HOME/.bash_profile
 source $HOME/.bashrc
 source $HOME/.cargo/env
 
+
+# install leo
 if ! [ -d /root/leo ]; then
     cd && git clone https://github.com/AleoHQ/leo.git
 fi
 cd $HOME/leo
 cargo install --path .
 
+# keys
+if [ -z "$PK" ]; then
+    read -p "Enter Your Private Key: " "PK"
+    echo 'export PK='$PK >> $HOME/.bashrc
+fi
+if [ -z "$VK" ]; then
+    read -p "Enter Your View Key: " VK
+    echo 'export VK='$VK >> $HOME/.bashrc
+fi
+if [ -z "$ADDRESS" ]; then
+    read -p "Enter Your Address: " ADDRESS
+    echo 'export ADDRESS='$ADDRESS >> $HOME/.bashrc
+fi
+
 # contract name
-read -p "Enter Your Contract Name: " NAME
-echo 'export NAME='${NAME} >> $HOME/.bash_profile
-source $HOME/.bash_profile
+enter_val "Enter Your Contract Name: " NAME
 
 if ! [ -d /root/leo_deploy ]; then
     mkdir $HOME/leo_deploy
@@ -51,19 +52,19 @@ fi
 cd $HOME/leo_deploy
 leo new $NAME
 
-# tx link
-read -p "Link: " QUOTE_LINK
-echo 'export QUOTE_LINK='${QUOTE_LINK} >> $HOME/.bash_profile
-source $HOME/.bash_profile
-
-CIPHERTEXT=$(curl -s "$QUOTE_LINK" | jq -r '.execution.transitions[0].outputs[0].value')
-
-RECORD=$(snarkos developer decrypt --ciphertext $CIPHERTEXT --view-key $VK)
 
 
 #################################### Deploy ####################################
 
-source $HOME/.bash_profile
+# Link
+if [ -z "$QUOTE_LINK" ]; then
+    enter_val "Enter Your Link: " QUOTE_LINK
+    echo 'export QUOTE_LINK='$QUOTE_LINK >> $HOME/.bashrc
+fi
+
+CIPHERTEXT=$(curl -s "$QUOTE_LINK" | jq -r '.execution.transitions[0].outputs[0].value')
+RECORD=$(snarkos developer decrypt --ciphertext $CIPHERTEXT --view-key $VK)
+
 snarkos developer deploy "$NAME.aleo" \
 --private-key "$PK" \
 --query "https://vm.aleo.org/api" \
@@ -72,16 +73,16 @@ snarkos developer deploy "$NAME.aleo" \
 --fee 4000000 \
 --record "$RECORD"
 
+read -p "Enter Deployment TX Hash: " DH
+QUOTE_LINK="https://vm.aleo.org/api/testnet3/transaction/"$DH
+echo 'export QUOTE_LINK='$QUOTE_LINK >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
 
 
 #################################### Execute ####################################
 
-read -p "Enter Deployment TX Hash: " DH
-DL="https://vm.aleo.org/api/testnet3/transaction/"$DH
-echo 'export DL='$DL >> $HOME/.bash_profile
-source $HOME/.bash_profile
-
-CIPHERTEXT=$(curl -s $DL | jq -r '.fee.transition.outputs[].value')
+CIPHERTEXT=$(curl -s $QUOTE_LINK | jq -r '.fee.transition.outputs[].value')
 RECORD=$(snarkos developer decrypt --ciphertext $CIPHERTEXT --view-key $VK)
 
 
@@ -91,3 +92,12 @@ snarkos developer execute "$NAME.aleo" "hello" "1u32" "2u32" \
 --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" \
 --fee 4000000 \
 --record "$RECORD"
+
+read -p "Enter Execution TX Hash Or Link: " EH
+QUOTE_LINK="https://vm.aleo.org/api/testnet3/transaction/"$EH
+echo 'export QUOTE_LINK='$QUOTE_LINK >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+
+# Scan
+# snarkOS developer scan --endpoint https://vm.aleo.org/api --start 50000 --end 104550 --view-key $VK

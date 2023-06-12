0085b859909cf96c4188a7c18d932eda2f5b2c35
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-06-12 18:54:44 +0700

    Update starknet_sc.sh

--- starknet/starknet_sc.sh
+++ starknet/starknet_sc.sh
@@ -1,27 +1,61 @@
 #!/bin/bash
 
-if [ -f ~/.bashrc ]; then $profile=".bashrc"
-else $profile=".bash_profile"
-fi
+######################## FUNCTIONS ########################
+
+enter_val(){
+    v=''
+    until [ ${#v} -gt 0 ]
+    do
+        read -p "Enter Your $1: " v
+    done
+    ${2}=$v
+
+}
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if ! [ -f ~/.bashrc ]; then sudo touch $HOME/.bashrc ;fi
+
 
 # load functions
-if ! [ -f /root/fn.sh ]; then
-    wget -O fn.sh https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh
-fi
-#curl -S fn.sh https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh
+if ! [ -f /root/fn.sh ]; then wget -O fn.sh https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh ;fi
+#curl https://raw.githubusercontent.com/sm-stranger/nodes-guides-and-scripts/main/fn.sh | bash
 source fn.sh
 
 # update && upgrade
+echo '============= UPGRADE ===========' && sleep 1
 sudo apt update && sudo apt upgrade -y
 
 # install dependencies
 sudo apt install mc -y
 
-# install Protostar
-curl -L https://raw.githubusercontent.com/software-mansion/protostar/master/install.sh | bash
+# Enter Private Key
+if [ -z "$PK" ]; then
+    read -p "Private Key: " PK
+    echo 'export PK='$PK >> $HOME/.bashrc
+    source $HOME/.bashrc
+fi
 
-# update profile
-source $HOME/$profile
+# Enter Address
+if [ -z "$ADDRESS" ]; then
+    read -p "Address: " ADDRESS
+    echo 'export ADDRESS='$ADDRESS >> $HOME/.bashrc
+    source $HOME/.bashrc
+fi
+
+# install Protostar
+if ! [ -d /root/.protostar ]; then
+    curl -L https://raw.githubusercontent.com/software-mansion/protostar/master/install.sh | bash
+fi
+source $HOME/.bashrc
+
+# install Scarb
+if exists scarb; then echo ''
+else curl --proto '=https' --tlsv1.2 -sSf https://docs.swmansion.com/scarb/install.sh | sh
+fi
 
 # enter project name
 read -p "Project Name:" NAME
@@ -33,21 +67,10 @@ protostar init $NAME
 cd $NAME
 
 # build
-protostar build $NAME
-
-# Enter Private Key
-read -p "Private Key: " PK
-echo 'export PK='$PK >> $HOME/$profile
-source $HOME/$profile
+protostar build --contract-name $NAME
 
 # record private key in .env
 echo $PK > .env
-
-# Enter Address
-read -p "Address: " ADDRESS
-echo 'export ADDRESS='$ADDRESS >> $HOME/$profile
-source $HOME/$profile
-
 
 
 # declare contract

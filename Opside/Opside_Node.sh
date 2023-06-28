0628fea2f410cf4c0d9615431e0bc1dd35b3b81f
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-06-28 18:42:14 +0700

    Update Opside_Node.sh

--- Opside/Opside_Node.sh
+++ Opside/Opside_Node.sh
@@ -1,4 +1,4 @@
-###################################### INSTALL ######################################
+
 
 # update && upgrade
 sudo apt update && sudo apt upgrade -y
@@ -9,14 +9,41 @@ tar -C ./ -xzf testnet-auto-install-v2.tar.gz
 chmod +x -R ./testnet-auto-install-v2
 cd ./testnet-auto-install-v2
 
-# install validator
-./install-ubuntu-en-1.0.sh
+
+Get_Key_Parameters
+
+
+###################################### WALLET ######################################
+
+PS3="Generate New Wallet Or Restore Existing?"
+options=("Generate New" "Restore")
+select opt in "${options[@]}"
+do
+	case $opt in
+		
+		"Generate New")
+			./opside-chain/tools/deposit --language 3 new-mnemonic --mnemonic_language 4 --num_validators 1 --chain testnet --eth1_withdrawal_address $withdrawal --keystore_password $password
+
+		break
+		;;
+
+		"Restore")
+			./opside-chain/tools/deposit --language 3 existing-mnemonic --mnemonic_language 4 --num_validators 1 --chain testnet --eth1_withdrawal_address $withdrawal --keystore_password $password			
+		break
+		;;
+
+	esac
+
+done
+
+
+Init_Jwt_Key
+Start_Geth
+Start_BeaconChain
+Start_Validator
 
 
 
-###################################### RESTORE ######################################
-
-./opside-chain/tools/deposit --language 3 existing-mnemonic --mnemonic_language 4 --num_validators 1 --chain testnet --eth1_withdrawal_address $withdrawal --keystore_password $password
 
 
 
@@ -32,7 +59,3 @@ opside-chain/show-beaconChain-log.sh
 opside-chain/show-validator-log.sh
 
 
-
-Restore_Deposit_Data(){
-	./opside-chain/tools/deposit --language 3 existing-mnemonic --mnemonic_language 4 --num_validators 1 --chain testnet --eth1_withdrawal_address $withdrawal --keystore_password $password
-}

70e034a0a3a66b4a5441963684dc29b9fc645628
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-07-02 16:44:31 +0700

    Update Opside_Node.sh

--- Opside/Opside_Node.sh
+++ Opside/Opside_Node.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if ! [ -d /root/./testnet-auto-install-v2 ]; then
+if ! [ -d /root/testnet-auto-install-v3 ]; then
     wget -c https://pre-alpha-download.opside.network/testnet-auto-install-v2.tar.gz
     tar -C ./ -xzf testnet-auto-install-v3.tar.gz
     chmod +x -R ./testnet-auto-install-v3
 fi;
 
-cd ./testnet-auto-install-v3
-./install-ubuntu-en-1.0.sh
+cd testnet-auto-install-v3
+./install-ubuntu-1.0.sh

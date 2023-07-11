5e3aff664373533be68b06a8b7109636832a78f7
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-07-11 17:17:27 +0700

    Update starknet_sc.sh

--- starknet/Smart_Contract/starknet_sc.sh
+++ starknet/Smart_Contract/starknet_sc.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-######################## FN ########################
-
-enter_val(){
-    v=''
-    until [ ${#v} -gt 0 ]
-    do
-        read -p "Enter Your $1: " v
-    done
-    ${2}=$v
-
-}
-
-exists()
-{
-  command -v "$1" >/dev/null 2>&1
-}
-
-######################## END OF FN ########################
-
-
 
 if ! [ -f ~/.bashrc ]; then sudo touch $HOME/.bashrc ;fi
 
@@ -37,30 +17,20 @@ sudo apt update && sudo apt upgrade -y
 # install dependencies
 sudo apt install mc -y
 
-# Enter Private Key
-if [ -z "$PK" ]; then
-    read -p "Private Key: " PK
-    echo 'PK='$PK >> $HOME/.bashrc
-    source $HOME/.bashrc
-fi
-
-# Enter Address
-if [ -z "$ADDRESS" ]; then
-    read -p "Address: " ADDRESS
-    echo 'ADDRESS='$ADDRESS >> $HOME/.bashrc
-    source $HOME/.bashrc
-fi
 
 # install Protostar
-if ! [ -d /root/.protostar ]; then
-    curl -L https://raw.githubusercontent.com/software-mansion/protostar/master/install.sh | bash && source $HOME/.bashrc
-fi
+if ! [ -d /root/.protostar ]; then curl -L https://raw.githubusercontent.com/software-mansion/protostar/master/install.sh | bash && source $HOME/.bashrc ;fi
 
 # install Scarb
 if exists scarb; then echo ''
-else
-    curl --proto '=https' --tlsv1.2 -sSf https://docs.swmansion.com/scarb/install.sh | sh && source $HOME/.bashrc && scarb --version
+else curl --proto '=https' --tlsv1.2 -sSf https://docs.swmansion.com/scarb/install.sh | sh && source $HOME/.bashrc && scarb --version
 fi
+
+# Enter Private Key
+if [ -z "$PK" ]; then read -p "Private Key: " PK && echo 'PK='$PK >> $HOME/.bashrc ;fi
+
+# Enter Address
+if [ -z "$ADDRESS" ]; then read -p "Address: " ADDRESS && echo 'ADDRESS='$ADDRESS >> $HOME/.bashrc ;fi
 
 source $HOME/.bashrc
 

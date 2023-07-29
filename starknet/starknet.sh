b1283a015eafe501ab02b0e2699241f599b9eeba
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-07-29 18:58:41 +0700

    Update starknet.sh

--- starknet/starknet.sh
+++ starknet/starknet.sh
@@ -33,7 +33,7 @@ do
                 git clone https://github.com/eqlabs/pathfinder.git
                 cd pathfinder
                 git fetch
-                git checkout v0.6.7
+                git checkout v0.7.0
                 cd $HOME/pathfinder/py
                 python3.10 -m venv .venv
                 source .venv/bin/activate
@@ -107,7 +107,7 @@ do
                 rustup update
                 git pull
                 git fetch --all
-                git checkout v0.6.7
+                git checkout v0.7.0
                 source $HOME/.cargo/env
                 cargo build --release --bin pathfinder
                 mv ~/pathfinder/target/release/pathfinder /usr/local/bin/

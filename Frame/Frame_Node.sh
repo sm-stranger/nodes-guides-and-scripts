2a5ca444afa606c0d173cc9f3b60bd26533b3b36
sm-stranger <128177926+sm-stranger@users.noreply.github.com>
2023-11-23 16:12:21 +0700

    Create Frame_Node.sh

---
+++ Frame/Frame_Node.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+colors{
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+
+ line{
+  echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
+}
+
+ install_docker{
+    if ! type "docker" > /dev/null; then
+        echo -e "${YELLOW}Install Docker${NORMAL}"
+        bash <(curl -s https://raw.githubusercontent.com/F1rstCap1tal/doker/main/doker.sh)
+    fi
+}
+
+ prepare_files{
+    echo -e "${YELLOW}Prepare Config Files${NORMAL}"
+    if [ ! -d "$HOME/frame-validator" ]; then
+        mkdir -p $HOME/frame-validator/node-data
+        cd $HOME/frame-validator
+        if [ ! -d "$HOME/frame-validator/node-config" ]; then
+            git clone https://github.com/frame-network/node-config.git
+            sed -i 's|"url":.*|"url": "https://ethereum-sepolia.publicnode.com"|' node-config/testnet.json
+        fi
+    else
+        cd $HOME/frame-validator
+        docker rm -f frame
+        rm -rf $HOME/frame-validator/node-config
+        git clone https://github.com/frame-network/node-config.git
+        sed -i 's|"url":.*|"url": "https://ethereum-sepolia.publicnode.com"|' node-config/testnet.json
+    fi
+
+}
+
+ run_docker{
+    echo -e "${YELLOW}Run Validator${NORMAL}"
+    if [ ! "$(docker ps -q -f name=^frame$)" ]; then
+        if [ "$(docker ps -aq -f status=exited -f name=^frame$)" ]; then
+            docker rm -f frame
+        fi
+        docker run -d --name frame --restart always -it -v $(pwd)/node-data:/home/user/.frame -v $(pwd)/node-config/testnet.json:/home/user/testnet.json public.ecr.aws/o8e2k8j7/nitro-node:frame --conf.file testnet.json
+    fi
+
+}
+
+
+ output{
+    echo -e "${YELLOW}Для проверки логов выполняем команду:${NORMAL}"
+    echo -e "docker logs -f frame --tail=100"
+    echo -e "${YELLOW}Для перезапуска выполняем команду:${NORMAL}"
+    echo -e "docker restart frame"
+}
+
+ main{
+    colors
+    line
+    prepare_files
+    line
+    install_docker
+    line
+    run_docker
+    line
+    output
+    line
+}
+
+#main
+
+PS3="Choose Option And Press Enter: "
+options=(
+    
+    "Commands"
+)

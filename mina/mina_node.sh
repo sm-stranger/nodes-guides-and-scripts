89bdd956f74160d6843e9b00027105c39059c1cd
Some Stranger <128177926+sm-stranger@users.noreply.github.com>
2024-07-27 01:51:22 +0700

    Update mina_node.sh

--- mina/mina_node.sh
+++ mina/mina_node.sh
@@ -8,6 +8,15 @@ orange='\033[93m'
 
 dash="============================================================="
 
+
+###################################### Prepare ######################################
+
+# update && upgrade
+sudo apt update && sudo apt upgrade && sudo apt install mc -y
+
+# install Docker
+sudo apt install docker.io curl -y && sudo systemctl start docker && sudo systemctl enable docker
+
 while true do
 
 
@@ -21,10 +30,11 @@ while true do
     PS3="Choose option and press Enter: "
     options=(
         "Install"
-        "Remove"
         "Run"
-        "Stop"
-        "Status"
+        "Check Status"
+        "Import Account"
+        "Unlock Account"
+        "Send Payment"
         )
         
    select opt in "${options[@]}"
@@ -32,31 +42,26 @@ while true do
       case $opt in
 
 
-         ######################################## Install Mina ########################################
+         ######################################## Install ########################################
          "Install")
 
             if ! [ -d /root/.mina-config ]; then
-               
-               # update && upgrade
-               sudo apt update && sudo apt upgrade
 
                # install Mina
                sudo rm /etc/apt/sources.list.d/mina*.list
                sudo echo "deb [trusted=yes] http://packages.o1test.net $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/mina.list
                sudo apt update
-               sudo apt install --allow-downgrades -y mina-mainnet=3.0.0-93e0279
-               sudo apt-get install -y curl unzip mina-mainnet=3.0.0-93e0279
+               sudo apt install -y curl unzip mina-mainnet=3.0.0-93e0279
 
-               # set permission
-               chmod 700 $HOME/keys && chmod 600 $HOME/keys/my-wallet
+               if [ -d /keys ]
+                  # set permission
+                  chmod 700 $HOME/keys && chmod 600 $HOME/keys/my-wallet
 
-               # export keys
-               echo 'export KEYPATH=$HOME/keys/my-wallet' >> $HOME/.bashrc
-               echo 'export MINA_PUBLIC_KEY=$(cat $HOME/keys/my-wallet.pub)' >> $HOME/.bashrc
-               source ~/.bashrc
-
-               # install Docker
-               sudo apt install docker.io curl -y && sudo systemctl start docker && sudo systemctl enable docker
+                  # export keys
+                  echo 'export KEYPATH=$HOME/keys/my-wallet' >> $HOME/.bashrc
+                  echo 'export MINA_PUBLIC_KEY=$(cat $HOME/keys/my-wallet.pub)' >> $HOME/.bashrc
+                  source ~/.bashrc
+               fi
 
                # open ports 8302 and 8303
                sudo iptables -A INPUT -p tcp --dport 8302:8303 -j ACCEPT
@@ -68,7 +73,7 @@ while true do
 
          
          
-         ######################################## Run Mina ########################################
+         ######################################## Run ########################################
          "Run")
 
             sudo docker run --name mina -d \
@@ -89,6 +94,43 @@ while true do
          ;;
 
 
+         ######################################## Check Status ########################################
+         "Check Status")
+
+            sudo docker exec -it mina mina client status
+
+         break
+         ;;
+
+
+         ######################################## Import Account ########################################
+         "Import Account")
+            mina accounts import -privkey-path $KEYPATH
+         break
+         ;;
+
+
+         ######################################## Unlock Account ########################################
+         "Unlock Account")
+            mina accounts unlock -public-key $MINA_PUBLIC_KEY
+         break
+         ;;
+
+
+         ######################################## Send Payment ########################################
+         "Send Payment")
+            
+            read -p "Enter Receiver Of Payment: " receiver
+            read -p "Enter Amount To Send: " amount
+
+            mina client send-payment \
+            -sender B62qjU8FUq5CmJ6MWFFEM1KGVb9DfZSvSmDCD1U5vgxrpDxAV3JZDrj \
+            -receiver $receiver \
+            -fee 0.01 \
+            -amount $amount
+
+         break
+         ;;
 
 
       esac
@@ -97,64 +139,32 @@ while true do
 done
 
 
-# 2.1 Запуск только Производителя блоков (Block Producer):(одна команда. Все строки)
-
-
-# импорт аккаунта(пароли с флеши)
-mina accounts import -privkey-path $KEYPATH
-
-
-# разблокировка аккаунта (пароли с флеши)
-mina accounts unlock -public-key $MINA_PUBLIC_KEY
-
-
-# отправить платеж (одна команда. Все строки)
-mina client send-payment \
--sender B62qjU8FUq5CmJ6MWFFEM1KGVb9DfZSvSmDCD1U5vgxrpDxAV3JZDrj \
--receiver B62qp75GRmMaN1B6MkuDPZiJEvDr1vmTyswwyzyom2c2rZp5f73BDmG \
--fee 0.01 \
--amount 1931
 
 
 
 ######################## Просмотр логов ########################
 
 # Посмотреть запущенные контейнеры:
-sudo docker ps -a
+#sudo docker ps -a
 
 # Логи контейнера с нодой:
-sudo docker logs --follow mina -f --tail 1000
+#sudo docker logs --follow mina -f --tail 1000
 
 # Статус ноды:
-sudo docker exec -it mina mina client status
+#sudo docker exec -it mina mina client status
 
 
 
 ######################## Команды Докера ########################
 
 # Остановка контейнера:
-sudo docker stop mina
+#sudo docker stop mina
 
 # Рестарт контейнера
 sudo docker restart mina
 
 # Удаление контейнера:
-sudo docker rm mina
+#sudo docker rm mina
 
 # Удаление запущенного контейнера:
-sudo docker rm -f mina
-
-
-sudo docker run --name mina -d \
---restart always \
--p 8302:8302 \
--p 127.0.0.1:3085:3085 \
--v $(pwd)/keys:/root/keys:ro \
--v $(pwd)/.mina-config:/root/.mina-config \
-minaprotocol/mina-daemon:3.0.0-93e0279-focal-mainnet daemon \
---peer-list-url https://storage.googleapis.com/mina-seed-lists/mainnet_seeds.txt \
---snark-worker-fee 0.025 \
---run-snark-worker $MINA_PUBLIC_KEY \
---insecure-rest-server \
---file-log-level Debug \
--log-level Info
+#sudo docker rm -f mina
